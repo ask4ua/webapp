dde288d33da285aaca8bc0eced27075ec0efc096
ask4ua <menia.zdes.net@gmail.com>
2020-09-18 19:22:10 +0300

    Delete build_all.sh

--- build_all.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cd web
-docker build ./ -t vovolkov/webapp:latest
-docker push vovolkov/webapp:latest
-docker build ./ -t vovolkov/webapp:fix -f Dockerfile-fix
-docker push vovolkov/webapp:fix
-cd ../db
-docker build ./ -t vovolkov/db
-docker push vovolkov/db
